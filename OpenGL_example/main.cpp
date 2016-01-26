6b6343a604ce4c721a5268c243062b6b277d6e06
Luwei Yang <corsy@me.com>
2016-01-26 12:11:43 -0800

    Initial Commit

---
+++ OpenGL_example/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL_example
+//
+//  Created by Luwei Yang on 1/26/16.
+//  Copyright © 2016 Luwei Yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
